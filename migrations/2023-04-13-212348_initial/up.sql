dd2373b5cb321ef421f4ce2600afd58371a08a84
dolanske <dolanovsky@gmail.com>
2023-04-15 01:55:26 +0300

    wip: sql schema

--- migrations/2023-04-13-212348_initial/up.sql
+++ migrations/2023-04-13-212348_initial/up.sql
@@ -8,3 +8,32 @@ CREATE TABLE users (
     username TEXT PRIMARY KEY NOT NULL COLLATE NOCASE,
     created_at INTEGER NOT NULL -- unix ts
 ) STRICT;
+
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NULL,
+    color TEXT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    location_lng REAL NULL,
+    location_lat REAL NULL,
+) STRICT;
+
+CREATE TABLE event_guests (
+    id INTEGER PRIMARY KEY NOT NULL,
+    event_id INTEGER NOT NULL,
+    guest_name TEXT NOT NULL,
+
+    UNIQUE(event_id, guest_name),
+
+    CONSTRAINT fk_guest_assoc
+        FOREIGN KEY (guest_name)
+        REFERENCES users (username)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_event_id_assoc
+        FOREIGN KEY (event_id)
+        REFERENCES events (id)
+        ON DELETE CASCADE
+) STRICT;
